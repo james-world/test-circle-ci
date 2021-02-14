424c9c9091f64e7b9b0fb79dcd6e8712670bc724
James World <james-world@users.noreply.github.com>
2021-02-14 13:00:06 +0000

    Implement Program

--- src/Adder/Program.cs
+++ src/Adder/Program.cs
@@ -2,11 +2,15 @@
 
 namespace Adder
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            
+            var parser = new Parser();
+            var calculator = new Calculator();
+            var (a, b) = parser.Parse(args);
+            var result = calculator.Add(a, b);
+            Console.WriteLine(result);
         }
     }
 }
